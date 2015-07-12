5204977870fec44a23b628f2f7f289caced87dcd
Zachary Palchick <zpalchick@5amsolutions.com>
2015-07-12 14:23:42 -0700

    Adds canonicalization of address and emails

--- hackerrank/fraud.py
+++ hackerrank/fraud.py
@@ -1,9 +1,20 @@
+STATE_MAPPINGS = {
+    'california': 'ca',
+    'illinois': 'il',
+    'new york': 'ny'
+}
+
 class Order:
+
     def __init__(self, csv_record):
         csv_values = csv_record.split(',')
         self._order_id = int(csv_values[0])
         self._deal_id = int(csv_values[1])
         self._email = csv_values[2]
+        self._street_address = csv_values[3]
+        self._city = csv_values[4]
+        self._state = csv_values[5]
+        self._zipcode = csv_values[6]
 
     @property
     def order_id(self):
@@ -18,4 +29,20 @@ class Order:
         canonical_email = self._email.lower()
         username, domain = canonical_email.split('@')
         username, _, _ = username.partition('+')
+        username = username.replace('.', '')
         return '{}@{}'.format(username, domain)
+
+    @property
+    def canonical_address(self):
+        if self._state.lower() in STATE_MAPPINGS:
+            state = STATE_MAPPINGS[self._state.lower()]
+        else:
+            state = self._state
+        zipcode = self._zipcode.replace('-','')
+        canonical_address = '{},{},{},{}'.format(
+            self._street_address,
+            self._city,
+            state,
+            zipcode
+        )
+        return canonical_address.lower()
--- test/fraud_test.py
+++ test/fraud_test.py
@@ -11,14 +11,32 @@ class OrderShould(unittest.TestCase):
         self.test_order_id = 1
         self.test_deal_id = 3
         self.test_email = 'bugs@bunny.com'
+        self.test_street_address = '123 sesame street'
+        self.test_city = 'chevy chase'
+        self.test_state = 'ca'
+        self.test_zipcode = '123456789'
+        self.test_address = '{},{},{},{}'.format(
+                self.test_street_address,
+                self.test_city,
+                self.test_state,
+                self.test_zipcode
+        )
         def get_order(
                 order_id = self.test_order_id,
                 deal_id = self.test_deal_id,
-                email = self.test_email):
-            csv_record = '{},{},{}'.format(
+                email = self.test_email,
+                street_address = self.test_street_address,
+                city = self.test_city,
+                state = self.test_state,
+                zipcode = self.test_zipcode):
+            csv_record = '{},{},{},{},{},{},{}'.format(
                 order_id,
                 deal_id,
-                email
+                email,
+                street_address,
+                city,
+                state,
+                zipcode
             )
             return Order(csv_record)
         self.get_order = get_order
@@ -36,24 +54,89 @@ class OrderShould(unittest.TestCase):
     @istest
     def parse_canonical_email_address(self):
         order = self.get_order()
-        self.assertEquals(self.test_email, order.canonical_email)
+        self.assertEqual(self.test_email, order.canonical_email)
 
     @istest
     def have_lowercase_canonical_email_address(self):
         order = self.get_order(email='BuGs@BunNy.CoM')
-        self.assertEquals(self.test_email, order.canonical_email)
+        self.assertEqual(self.test_email, order.canonical_email)
 
     @istest
     def strip_plus_sign_suffixes_from_email_username(self):
         order = self.get_order(email='bugs+1@bunny.com')
-        self.assertEquals(self.test_email, order.canonical_email)
+        self.assertEqual(self.test_email, order.canonical_email)
 
         order = self.get_order(email='bugs+1moreStuff@bunny.com')
-        self.assertEquals(self.test_email, order.canonical_email)
+        self.assertEqual(self.test_email, order.canonical_email)
 
     @istest
     def not_strip_plus_sign_suffixes_from_email_domain(self):
-        ok_email = 'bugs@bun+ny.com'
-        order = self.get_order(email=ok_email)
-        self.assertEquals(ok_email, order.canonical_email)
+        expected_email = 'bugs@bun+ny.com'
+        order = self.get_order(email=expected_email)
+        self.assertEqual(expected_email, order.canonical_email)
 
+    @istest
+    def ignore_periods_in_email_username(self):
+        order = self.get_order(email='b..u.gs@bunny.com')
+        self.assertEqual(self.test_email, order.canonical_email)
+
+        order = self.get_order(email='bu.gs+1moreS.tuff@bunny.com')
+        self.assertEqual(self.test_email, order.canonical_email)
+
+    @istest
+    def not_ignore_periods_in_email_domain(self):
+        expected_email = 'bugs@bun.ny.com'
+        order = self.get_order(email=expected_email)
+        self.assertEqual(expected_email, order.canonical_email)
+
+    @istest
+    def parse_canonical_address(self):
+        order = self.get_order()
+        self.assertEqual(
+            self.test_address, order.canonical_address)
+
+    @istest
+    def have_lower_case_canonical_address(self):
+        order = self.get_order(
+            street_address = '123 sEsaMe STReEt',
+            city = 'CheVy ChaSE',
+            state = 'Ca'
+        )
+        self.assertEqual(
+            self.test_address, order.canonical_address)
+
+    @istest
+    def strip_dashes_from_the_zipcode(self):
+        order = self.get_order(zipcode='12-34--5-67-89-')
+        self.assertEqual(
+            self.test_address, order.canonical_address)
+
+    @istest
+    def map_states_to_canonical_form(self):
+        order = self.get_order(state='California')
+        self.assertEqual(
+            self.test_address, order.canonical_address)
+
+        order = self.get_order(state='CaLiforniA')
+        self.assertEqual(
+            self.test_address, order.canonical_address)
+
+        order = self.get_order(state='Illinois')
+        expected_address = '{},{},{},{}'.format(
+            self.test_street_address,
+            self.test_city,
+            'il',
+            self.test_zipcode
+        )
+        self.assertEqual(
+            expected_address, order.canonical_address)
+
+        order = self.get_order(state='New York')
+        expected_address = '{},{},{},{}'.format(
+            self.test_street_address,
+            self.test_city,
+            'ny',
+            self.test_zipcode
+        )
+        self.assertEqual(
+            expected_address, order.canonical_address)
